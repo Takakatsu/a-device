7c73ab2f1f75bf46b5515bf603dffa4708b9bdf9
Takakatsu <taka5katsu28@outlook.jp>
2022-11-12 01:02:33 +0900

    keisyou

--- Immersion/Main.cpp
+++ Immersion/Main.cpp
@@ -5,17 +5,19 @@ void Main()
 	//ウィンドウサイズ等の初期設定
 	Window::Resize(1280, 720);
 
-	Array<MyWindow> my_wins;
+	Array<MyWindow*> my_wins;
 	MyWindow win = MyWindow(Vec2(0, 0), Vec2(100, 100));
-	my_wins.push_back(win);
+	Calculator cal = Calculator(Vec2(150, 0), Vec2(100, 100));
+	my_wins.push_back(&win);
+	my_wins.push_back(&cal);
 
 	while (System::Update())
 	{
 		Scene::SetBackground(Color(255));
 		for (int i = 0; i < my_wins.size(); i++)
 		{
-			my_wins[i].update();
-			my_wins[i].draw();
+			my_wins[i]->update();
+			my_wins[i]->draw();
 		}
 	}
 }
--- Immersion/MyWindow.h
+++ Immersion/MyWindow.h
@@ -15,3 +15,19 @@ public:
 	void setPos(Vec2);
 	void setSize(Vec2);
 };
+
+class Calculator : public MyWindow
+{
+private:
+protected:
+public:
+	Calculator() :MyWindow() {};
+	Calculator(Vec2 p, Vec2 s) : MyWindow(p, s) {};
+	void update()
+	{
+	}
+	void draw()
+	{
+		RectF(pos, size).draw(Color(127));
+	}
+};
